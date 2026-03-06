064fb313ee2ed641633f42421ded30d65523a7f2
Nhat Nam <trannhatnam891@gmail.com>
2026-03-06 23:52:42 +0700

    Delete some resources

--- src/resources.cpp
+++ src/resources.cpp
@@ -3,7 +3,7 @@
 
 
 void LoadGameAssets(GameAssets& assets) {
-    assets.bgMainMenu = LoadTexture("resources\\test3.png");
+    assets.bgMainMenu = LoadTexture("resources\\test4.png");
     assets.btnNormal = LoadTexture("resources\\Button\\Rect\\Fill\\Hover@0.5x.png");
     assets.btnPressed = LoadTexture("resources\\Button\\Rect\\Fill\\Default@0.5x.png");
     assets.gameFont = LoadFont("resources\\font\\JAPANESE_2020.ttf");
