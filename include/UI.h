1c5a16b672637dea30a5d821e1d7c77548003326
VuongLam <tankiensieusao@gmail.com>
2026-03-13 14:30:44 +0700

    Update UI.h
    thêm Silder cho setting

--- include/UI.h
+++ include/UI.h
@@ -1,15 +1,24 @@
 #ifndef UI_H
+#define UI_H
 #include "raylib.h"
-#include<vector>
-#include"resources.h"
+#include <vector>
+#include "resources.h"
+
 struct Button {
     Rectangle rect;
     const char* text;
 };
+
+struct Slider {
+    Rectangle barRect;
+    Rectangle knobRect;
+    float value;
+    bool isDragging;
+    const char* label;
+};
+
 void DrawMainMenu(int screenWidth, int screenHeight, int selectedIndex, const GameAssets& assets);
 void DrawSettingsMenu(int screenWidth, int screenHeight, GameSettings& settings, const GameAssets& assets, int& selectedOption, bool& backToMenu);
 void UpdateSettings(GameSettings& settings, int selectedOption, Vector2 mousePoint, bool mousePressed);
 
-
-#define UI_H
-#endif 
+#endif // UI_H
